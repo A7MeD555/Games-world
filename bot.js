488e085d36cf2d892f0e14d714236763e7962acb
A7MeD555 <43023294+A7MeD555@users.noreply.github.com>
2018-12-17 22:19:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,20 @@
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
- 
+
 client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`البوت تحت التطوير`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+client.user.setGame(`تحت التطوير`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
 });
 
 
