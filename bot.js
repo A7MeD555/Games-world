3bb9086b1c0901952c513001f330b473bccb2351
A7MeD555 <43023294+A7MeD555@users.noreply.github.com>
2018-12-20 19:37:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1035,11 +1035,21 @@ client.on("message", message => {
 client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(`Welcome To Games WorlD 
-  لاتنسا تفعل نفسك`) 
+  لاتنسا تفعل نفسك
+${member}`) 
 }).catch(console.error)
 })
 
-
+client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    invites[member.guild.id] = guildInvites;
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const logChannel = member.guild.channels.find(channel => channel.name === "ღ⇝welcome");
+    logChannel.send(`${member} Invited by: <@${inviter.id}>`);
+  });
+});
 
 
 
